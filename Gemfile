5fe6ce8d9cee3c49d72a064dbaf8f73c6c10ba89
Scott Reu <scott.reu@meraki.net>
2022-09-29 17:57:10 -0600

    Use class instance vars instead of constants & base64-encode config

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source 'https://rubygems.org'
 
 gem 'awesome_print'
 gem 'colorize'
-gem 'openssl'
 gem 'tty-prompt'
--- lib/cashier.rb
+++ lib/cashier.rb
@@ -6,19 +6,20 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 require 'awesome_print'
+require 'base64'
 require 'colorize'
-require 'tty-prompt'
 require 'json'
+require 'tty-prompt'
 
 prompt = TTY::Prompt.new
 
-CONFIG = {
-          debts: [],
-          expenses: 0.0,
-          target_remainder: 0.0,
-          pay_frequency: 2,
-          paycheck_amt: 0.0
-         }
+@config = {
+            debts: [],
+            expenses: 0.0,
+            target_remainder: 0.0,
+            pay_frequency: 2,
+            paycheck_amt: 0.0
+          }
 
 #
 # Get Balances
@@ -30,27 +31,46 @@ load_dotfile = prompt.yes?("Try to load cached settings from .cashier file?")
 if load_dotfile
   begin
     File.read('.cashier') do |file|
-      CONFIG = JSON.parse(file)
+      @config = JSON.parse(base64.decode64(file))
     end
   rescue Errno::ENOENT => e
     puts "No .cashier file found. Defaulting to manual input..."
     correct_balances = false
   end
+else
+  @config = {
+              debts: [],
+              expenses: 0.0,
+              target_remainder: 0.0,
+              pay_frequency: 2,
+              paycheck_amt: 0.0
+            }
 end
 
-RENT = prompt.ask('What are your mandatory recurring monthly expenses (e.g. rent) $', default: CONFIG[:expenses], convert: :float)
-TARGET = prompt.ask('How much money do you want to have leftover after expenses and payments? $', default: CONFIG[:target_remainder], convert: :float)
-INTERVAL = prompt.select("How frequently do you get paid?", default: CONFIG[:pay_frequency]) do |menu|
+if ENV['DEBUG']
+  puts "Read the following config from disk:"
+  ap @config
+end
+
+@rent = prompt.ask('What are your mandatory recurring monthly expenses (e.g. rent) $', default: @config[:expenses], convert: :float)
+@target = prompt.ask('How much money do you want to have leftover after expenses and payments? $', default: @config[:target_remainder], convert: :float)
+@interval = prompt.select("How frequently do you get paid?", default: @config[:pay_frequency]) do |menu|
   menu.choice "Every week", 1
   menu.choice "Every other week", 2
   menu.choice "Twice per month", 3
   menu.choice "Every month", 4
 end
-INCOME = prompt.ask('How much money is your paycheck usually for? $', default: CONFIG[:paycheck_amt], convert: :float)
+@income = prompt.ask('How much money is your paycheck usually for? $', default: @config[:paycheck_amt], convert: :float)
 
-unless CONFIG[:debts].empty?
+# Write updated values back to @config hash
+@config[:expenses] = @rent
+@config[:target_remainder] = @target
+@config[:pay_frequency] = @interval
+@config[:paycheck_amt] = @income
+
+unless @config[:debts].empty?
   puts "Found the following outstanding debts:"
-  ap CONFIG[:debts]
+  ap @config[:debts]
 
   correct_balances = prompt.yes?('Does this look correct?')
 end
@@ -64,11 +84,11 @@ while !correct_balances
   tmp_debt[:balance] = prompt.ask('How much is the outstanding balance on this debt? $', default: 0.0, convert: :float)
   tmp_debt[:minimum] = prompt.ask('How much is your minimum monthly payment on this debt? $', default: 0.0, convert: :float)
   tmp_debt[:payment] = nil
-  CONFIG[:debts].push tmp_debt
+  @config[:debts].push tmp_debt
   correct_balances = !prompt.yes?('Add another debt?')
 end
 
-CONFIG[:debts].each do |debt|
+@config[:debts].each do |debt|
   total_debt += debt[:balance]
 end
 
@@ -77,7 +97,7 @@ puts "Total Debt: #{total_debt.round(2)}".red.bold
 #
 # Get Available Cash
 #
-cash = prompt.ask("How much cash do you have on hand this week?", default: INCOME, convert: :float)
+cash = prompt.ask("How much cash do you have on hand this week?", default: @income, convert: :float)
 
 CASH_ON_HAND = if cash.zero?
                  4100.0
@@ -85,13 +105,13 @@ CASH_ON_HAND = if cash.zero?
                  cash.to_f
                end
 
-pool = CASH_ON_HAND - TARGET
+pool = CASH_ON_HAND - @target
 
 #
 # Sort debts by APR
 #
-debts = CONFIG[:debts].sort_by { |k| k[:apr] }
-CONFIG[:debts].reverse!
+debts = @config[:debts].sort_by { |k| k[:apr] }
+@config[:debts].reverse!
 
 #
 # Figure out if this paycheck has to cover rent as well as other 1st-of-the-month-type-things
@@ -105,12 +125,12 @@ if auto_rent
   first_of_next_month = Time.new(today.year, (today.month + 1), 1)
   if (first_of_next_month - today) <= 1209600
     puts "Your next paycheck needs rent money! Pay the rent. Removing rent money from available pool...".red
-    pool -= RENT
+    pool -= @rent
   end
 else
   subtract_rent = prompt.yes?('Subtract rent payment?')
   if subtract_rent
-    pool -= RENT
+    pool -= @rent
   end
 end
 
@@ -120,7 +140,7 @@ puts "\n\n"
 #
 # Check minimum payments
 #
-CONFIG[:debts].each do |debt|
+@config[:debts].each do |debt|
   minimum = prompt.ask("Enter any minimum payment due to #{debt[:creditor]} & not yet paid this month", default: debt[:minimum], convert: :float)
 
   pool -= minimum unless minimum.zero?
@@ -130,7 +150,7 @@ end
 
 if pool > 0.0
 # Allocate remainder to accounts in order of descending APR
-  CONFIG[:debts].each do |debt|
+  @config[:debts].each do |debt|
     break if pool <= 0
 
     if (debt[:balance] - debt[:payment]) <= pool
@@ -148,7 +168,7 @@ else
 end
 
 # List payments
-CONFIG[:debts].each do |debt|
+@config[:debts].each do |debt|
   puts "PAY #{debt[:creditor]}: #{debt[:payment].round(2)}".blue.bold
   total_debt -= debt[:payment]
   debt[:balance] -= debt[:payment]
@@ -158,4 +178,4 @@ puts "Projected debt after today's payments: $#{total_debt.to_i}".green.bold
 
 # Write balances to file
 puts "All done! Writing current state of finances to .cashier.".bold
-File.write('.cashier', JSON.dump(CONFIG))
+File.write('.cashier', Base64.encode64(JSON.dump(@config)))

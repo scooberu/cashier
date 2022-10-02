28cb211e59b9c64afe2a1243ab300f012d7a3846
Scott Reu <scott.reu@meraki.net>
2022-10-02 17:57:19 -0600

    First crack at calculating rent based on pay period

--- lib/cashier.rb
+++ lib/cashier.rb
@@ -10,15 +10,19 @@ require 'base64'
 require 'colorize'
 require 'json'
 require 'tty-prompt'
+require 'date'
+require 'time'
 
-prompt = TTY::Prompt.new
+SECONDS_IN_DAY = 86400
+
+@prompt = TTY::Prompt.new
 
 #
 # Get Balances
 #
 total_debt = 0.0
 
-load_dotfile = prompt.yes?("Try to load cached settings from .cashier file?")
+load_dotfile = @prompt.yes?("Try to load cached settings from .cashier file?")
 
 if load_dotfile
   begin
@@ -44,15 +48,15 @@ if ENV['DEBUG']
   ap @config
 end
 
-@rent = prompt.ask('What are your mandatory recurring monthly expenses (e.g. rent) $', default: @config[:expenses], convert: :float)
-@target = prompt.ask('How much money do you want to have leftover after expenses and payments? $', default: @config[:target_remainder], convert: :float)
-@interval = prompt.select("How frequently do you get paid?", default: @config[:pay_frequency]) do |menu|
+@rent = @prompt.ask('What are your mandatory recurring monthly expenses (e.g. rent) $', default: @config[:expenses], convert: :float)
+@target = @prompt.ask('How much money do you want to have leftover after expenses and payments? $', default: @config[:target_remainder], convert: :float)
+@interval = @prompt.select("How frequently do you get paid?", default: @config[:pay_frequency]) do |menu|
   menu.choice "Every week", 1
   menu.choice "Every other week", 2
   menu.choice "Twice per month", 3
   menu.choice "Every month", 4
 end
-@income = prompt.ask('How much money is your paycheck usually for? $', default: @config[:paycheck_amt], convert: :float)
+@income = @prompt.ask('How much money is your paycheck usually for? $', default: @config[:paycheck_amt], convert: :float)
 
 # Write updated values back to @config hash
 @config[:expenses] = @rent
@@ -64,20 +68,20 @@ unless @config[:debts].empty?
   puts "Found the following outstanding debts:"
   ap @config[:debts]
 
-  correct_balances = prompt.yes?('Does this look correct?')
+  correct_balances = @prompt.yes?('Does this look correct?')
 end
 
 correct_balances ||= false
 puts "Please enter any outstanding debts:".bold unless correct_balances
 while !correct_balances
   tmp_debt = {}
-  tmp_debt[:creditor] = prompt.ask('What is the name of this creditor?')
-  tmp_debt[:apr] = prompt.ask('What is the interest rate you are paying on this debt? [e.g.: 0.049 for 4.9%]', convert: :float)
-  tmp_debt[:balance] = prompt.ask('How much is the outstanding balance on this debt? $', default: 0.0, convert: :float)
-  tmp_debt[:minimum] = prompt.ask('How much is your minimum monthly payment on this debt? $', default: 0.0, convert: :float)
+  tmp_debt[:creditor] = @prompt.ask('What is the name of this creditor?')
+  tmp_debt[:apr] = @prompt.ask('What is the interest rate you are paying on this debt? [e.g.: 0.049 for 4.9%]', convert: :float)
+  tmp_debt[:balance] = @prompt.ask('How much is the outstanding balance on this debt? $', default: 0.0, convert: :float)
+  tmp_debt[:minimum] = @prompt.ask('How much is your minimum monthly payment on this debt? $', default: 0.0, convert: :float)
   tmp_debt[:payment] = nil
   @config[:debts].push tmp_debt
-  correct_balances = !prompt.yes?('Add another debt?')
+  correct_balances = !@prompt.yes?('Add another debt?')
 end
 
 @config[:debts].each do |debt|
@@ -89,15 +93,11 @@ puts "Total Debt: #{total_debt.round(2)}".red.bold
 #
 # Get Available Cash
 #
-cash = prompt.ask("How much cash do you have on hand this week?", default: @income, convert: :float)
+cash = @prompt.ask("How much cash do you have on hand this week? $", default: @income, convert: :float)
 
-CASH_ON_HAND = if cash.zero?
-                 4100.0
-               else
-                 cash.to_f
-               end
+CASH_ON_HAND = cash
 
-pool = CASH_ON_HAND - @target
+@pool = CASH_ON_HAND - @target
 
 #
 # Sort debts by APR
@@ -110,48 +110,73 @@ debts = @config[:debts].sort_by { |k| k[:apr] }
 #
 
 # Manual Check
-auto_rent = prompt.yes?('Use the auto-rent calculator to subtract rent from this payment (if necessary)?')
+auto_rent = @prompt.yes?('Use the auto-rent calculator to subtract rent from this payment (if necessary)?')
 
-if auto_rent
+
+def days_in_month(year, month)
+  Date.new(year, month, -1).day
+end
+
+def rent_calc(pay_interval)
   today = Time.now
+  days_in_current_month = days_in_month(today.year, today.month)
   first_of_next_month = Time.new(today.year, (today.month + 1), 1)
-  if (first_of_next_month - today) <= 1209600
-    puts "Your next paycheck needs rent money! Pay the rent. Removing rent money from available pool...".red
-    pool -= @rent
-  end
-else
-  subtract_rent = prompt.yes?('Subtract rent payment?')
-  if subtract_rent
-    pool -= @rent
+  rent_interval_in_seconds = case pay_interval
+                             when 1 # weekly
+                               SECONDS_IN_DAY * 7
+                             when 2 # every other week
+                               SECONDS_IN_DAY * 14
+                             when 3 # twice monthly
+                               ((SECONDS_IN_DAY * days_in_current_month) / 2).to_i
+                             when 4 # monthly
+                               SECONDS_IN_DAY * days_in_current_month
+                             else
+                               raise "Something went wrong with rent calculation! (Hit a segment of code we should never have hit; likely cause: invalid selection for paycheck interval)"
+                             end
+
+  if (first_of_next_month - today) <= rent_interval_in_seconds
+    puts "Your next paycheck has to cover rent money! Pay the rent. Removing rent money from available pool...".red
+    @pool -= @rent
+  else
+    puts "Your next rent payment is more than 1 pay period away; no need to factor it in here!".green
   end
 end
 
-puts "AVAILABLE CASH: $#{pool.round(2)}".green
+if auto_rent
+  rent_calc(@interval)
+else
+  subtract_rent = @prompt.yes?('Subtract rent payment from available cash for debt reduction?')
+  if subtract_rent
+    @pool -= @rent
+  end
+end
+
+puts "AVAILABLE CASH: $#{@pool.round(2)}".green
 puts "\n\n"
 
 #
 # Check minimum payments
 #
 @config[:debts].each do |debt|
-  minimum = prompt.ask("Enter any minimum payment due to #{debt[:creditor]} & not yet paid this month", default: debt[:minimum], convert: :float)
+  minimum = @prompt.ask("Enter any minimum payment due to #{debt[:creditor]} & not yet paid this month", default: debt[:minimum], convert: :float)
 
-  pool -= minimum unless minimum.zero?
+  @pool -= minimum unless minimum.zero?
 
   debt[:payment] = debt[:minimum]
 end
 
-if pool > 0.0
+if @pool > 0.0
 # Allocate remainder to accounts in order of descending APR
   @config[:debts].each do |debt|
-    break if pool <= 0
+    break if @pool <= 0
 
-    if (debt[:balance] - debt[:payment]) <= pool
-      puts "LOG: available cash ($#{pool}) is greater than the balance of #{debt[:creditor]} debt (less any planned minimum payments); recommend paying #{debt[:creditor]} the full balance of $#{debt[:balance].round(2)}." if ENV['DEBUG']
-      pool -= (debt[:balance] - debt[:payment])
+    if (debt[:balance] - debt[:payment]) <= @pool
+      puts "LOG: available cash ($#{@pool}) is greater than the balance of #{debt[:creditor]} debt (less any planned minimum payments); recommend paying #{debt[:creditor]} the full balance of $#{debt[:balance].round(2)}." if ENV['DEBUG']
+      @pool -= (debt[:balance] - debt[:payment])
       debt[:payment] = debt[:balance]
     else
-      debt[:payment] += pool
-      pool = 0.0
+      debt[:payment] += @pool
+      @pool = 0.0
     end
   end
 else
